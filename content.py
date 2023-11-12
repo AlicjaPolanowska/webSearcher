9bba747f9d0cca0c65e7300d01f051c729400b22
Alicja Polanowska <ala.polanowska@gmail.com>
2023-11-12 15:56:02 +0100

    punched  the wall -cannot get full page with only pure pythonic 'url get'

--- content.py
+++ content.py
@@ -1,11 +1,30 @@
 import urllib.request
+from html.parser import HTMLParser
+
+class HTMLFilter(HTMLParser):
+    text = ""
+    def handle_data(self, data):
+        self.text += data
 
 
 def read_url(url: str):
     with urllib.request.urlopen(url) as response:
         html = response.read()
-        return html.decode("utf-8")
+        try:
+            return html.decode("utf-8")
+        except:
+            return None
 
 
 def get_page_hrefs(page: str, splitter: str = '\n'):
+    if page is None:
+        return None
     return set(x[x.index('href="http')+6:x.index('"', x.index('href="http')+6)] for x in page.split(splitter) if x.find('href="http') > -1)
+
+
+def plain_text_from_html(page:str) -> str:
+    if page is None:
+        return None
+    f = HTMLFilter()
+    f.feed(page)
+    return f.text
--- main.py
+++ main.py
@@ -18,26 +18,39 @@ def graph_from_page(root: str, page: str):
     hrefs = content.get_page_hrefs(page)
     for y in hrefs:
         idx = graph.add_vertex(y, 0)
-        if idx == -1:
-            continue
-        page_inner = content.read_url(y)
-        hrefs_inner = content.get_page_hrefs(page_inner)
-        for z in hrefs_inner:
-            graph.add_vertex(z, idx)
+        # if idx == -1:
+        #     continue
+        # page_inner = content.read_url(y)
+        # hrefs_inner = content.get_page_hrefs(page_inner)
+        # for z in hrefs_inner:
+        #     graph.add_vertex(z, idx)
     return graph
+
+
+def find_occurrences(graph, target_value):
+    found = []
+    for current_vertex in graph:
+        current_content = content.plain_text_from_html(content.read_url(current_vertex.url))
+        if current_content is None:
+            continue
+        found_index = current_content.find(target_value)
+        if found_index != -1:
+            found.append(f'I found value {target_value} at page {current_vertex} at possition {found_index}. The text: {current_content[found_index:found_index+len(target_value)+10]}')
+    return found
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    url = 'http://python.org/'
+    url = 'https://www.onet.pl'
     page = content.read_url(url)
     g = graph_from_page(url, page)
-    start = time.time()
+    #start = time.time()
     print(g)
-    end = time.time()
-    print(end-start)
-    print(len(g.vertices))
+    #end = time.time()
+    #print(end-start)
+    #print(len(g.vertices))
+    # print(find_occurrences(g, 'Kaczyński'))
     #cProfile.run('print(g)')
     #0.0030341148376464844
     #dfs.run()
